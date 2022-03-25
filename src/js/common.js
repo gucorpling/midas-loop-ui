da9075613db771b0205afb42070913608519c384
Luke Gessler <lukegessler@gmail.com>
2022-03-25 14:41:23 -0400

    impl login flow
    don't enforce login for now

--- src/js/common.js
+++ src/js/common.js
@@ -1,1 +1,32 @@
 import 'bootstrap/dist/css/bootstrap.min.css';
+import '@fortawesome/fontawesome-free/js/fontawesome'
+import '@fortawesome/fontawesome-free/js/solid'
+import '@fortawesome/fontawesome-free/js/regular'
+import '@fortawesome/fontawesome-free/js/brands'
+import Api from '../js/api';
+
+
+"gold;secret=xxcCIyLMHc0IMd7_svZdjANQG5xV3bFum0lI23stHkw"
+
+function getCookie(key) {
+    const pieces = document.cookie.split(";");
+    console.log(pieces)
+    for (let piece of pieces) {
+        const [k, v] = piece.split("=");
+        if (k.trim() === key.trim()) {
+            return v;
+        }
+    }
+}
+
+// Ensure user has good credentials, otherwise redirect to login page
+async function checkAuth () {
+    const location = window.location.href;
+    const api = new Api();
+    let token = getCookie("token").replace("__SEMI__", ";").replace("__EQ__", "=");
+    const response = await api.checkToken(token);
+    if (!response.ok) {
+        //window.location.href = "/login.html";
+    }
+}
+checkAuth();
--- src/page-login/main.js
+++ src/page-login/main.js
@@ -1,7 +1,22 @@
 import '../css/main.css';
 import '../js/common.js';
-import $, { post } from 'jquery'
-import 'jquery-ui'
-import 'jquery-ui-bundle'
 import './page.css'
+import Api from '../js/api';
 
+const api = new Api();
+
+const input = document.getElementById("token");
+const msg = document.getElementById("bad-token");
+input.addEventListener("input", async function(e) {
+  const token = e.target.value;
+  const resp = await api.checkToken(token);
+  const validToken = resp.ok;
+  input.className = "form-control"
+  if (!validToken) {
+    msg.innerText = "Invalid token";
+    input.className = "form-control is-invalid"
+  } else {
+    document.cookie = "token=" + token.replace(";", "__SEMI__").replace("=", "__EQ__");
+    window.location.href = "/index.html"
+  }
+});
