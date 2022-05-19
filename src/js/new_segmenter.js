297b22c90ed11e07fdccaa3a6c1fac16b6a02028
Luke Gessler <lukegessler@gmail.com>
2022-05-18 20:02:00 -0400

    spacing fix; refinement to ui

--- src/js/new_segmenter.js
+++ src/js/new_segmenter.js
@@ -9,113 +9,119 @@ import { api } from '../js/common'
 import '../css/new_segmenter.css'
 
 export function Document(props) {
-	const [doc, setDoc] = useState(null);
-	const [busy, setBusy] = useState(false);
-	const [insideToken, setInsideToken] = useState(false);
-	const leaveToken = () => setInsideToken(false);
-	const enterToken = () => setInsideToken(true);
-  
-	useEffect(() => {
-	  (async () => {
-		const data = await api.getDocument(props.id);
-		await setDoc(data);
-	  })()
-	}, [props.id, busy])
-  
-	function Sentences(props) {
-	  return <>
-		<div>{props.sentences.map(Sentence)}</div>
-	  </>
-	}
-  
-	function Sentence(props) {
-	  async function merge(e) {
-		e.preventDefault()
-		if (!busy && !insideToken) {
-		  setBusy(true);
-		  const result = await api.mergeSentenceLeft(props.id);
-		  setBusy(false);
-		}
-	  }
-	  let className = "sentence"
-	  const firstToken = props.tokens && props.tokens.length > 0 && props.tokens[0]
-	  const outProb = firstToken && firstToken.probas && firstToken.probas.O
-	  const maybeMerge = outProb && outProb > 0.9
-	  const isGold = firstToken.quality && firstToken.quality === "gold"
-	  if (!isGold && maybeMerge) {
-		  className += " sentence-maybe-merge"
-	  }
-	  if (!insideToken) {
-		  className += " sentence--hoverable"
-	  }
-	  return (
-		  <div key={props.id} className={className} onClick={merge}>
-			<span className="sentence-icon"><Merge fontSize="small" /></span>
-			{props.tokens.map((v, i) => Token({...v, index: i}))}
-		  </div>
-	  )
-	}
-  
+  const [doc, setDoc] = useState(null);
+  const [busy, setBusy] = useState(false);
+  const [insideToken, setInsideToken] = useState(false);
+  const leaveToken = () => setInsideToken(false);
+  const enterToken = () => setInsideToken(true);
+
+  useEffect(() => {
+    (async () => {
+      const data = await api.getDocument(props.id);
+      await setDoc(data);
+    })()
+  }, [props.id, busy])
+
+  function Sentences(props) {
+    return <>
+      <div>{props.sentences.map(Sentence)}</div>
+    </>
+  }
+
+  function Sentence(props) {
+    async function merge(e) {
+      e.preventDefault()
+      if (!busy && !insideToken) {
+        setBusy(true);
+        const result = await api.mergeSentenceLeft(props.id);
+        setBusy(false);
+      }
+    }
+    let className = "sentence"
+    const firstToken = props.tokens && props.tokens.length > 0 && props.tokens[0]
+    const outProb = firstToken && firstToken.probas && firstToken.probas.O
+    const maybeMerge = outProb && outProb > 0.9
+    const isGold = firstToken.quality && firstToken.quality === "gold"
+    if (!isGold && maybeMerge) {
+      className += " sentence-maybe-merge"
+    }
+    if (!insideToken) {
+      className += " sentence--hoverable"
+    }
+    return (
+      <div key={props.id} className={className} onClick={merge}>
+        <span className="sentence-icon"><Merge fontSize="small" /></span>
+        {props.tokens.map((v, i) => Token({ ...v, index: i }))}
+      </div>
+    )
+  }
+
 	function Token(props) {
-	  async function split(e) {
-		e.preventDefault();
-		// Only proceed if another action isn't in progress
-		if (!busy) {
-		  setBusy(true);
-		  const result = await api.splitSentence(props.id);
-		  setBusy(false);
+		async function split(e) {
+			e.preventDefault();
+			// Only proceed if another action isn't in progress
+			if (!busy) {
+				setBusy(true);
+				const result = await api.splitSentence(props.id);
+				setBusy(false);
+			}
 		}
-	  }
-	  let tokenAreaClassName;
-	  const isGold = props.quality && props.quality === "gold"
-	  if (props.index > 0 && props.probas && props.probas.B > 0.1 && !isGold) {
-		tokenAreaClassName = "token-area token-area-maybe-split"
-	  } else {
-		tokenAreaClassName = "token-area"
-	  }
-	  return <span className={tokenAreaClassName}
-				   title="Split sentence"
-				   onClick={split}
-				   key={props.id}
-				   onMouseEnter={enterToken}
-				   onMouseLeave={leaveToken}>
-		{(props.index > 0) ? <> <span className="token-button"> <CallSplit fontSize="small" /></span></> : ""}
-		<span className="token">{props.form.value}</span>
-	  </span>
-	}
-  
-	async function download(e) {
-	  e.preventDefault();
-	  const result = await api.downloadConlluFile(props.id);
-	  const blob = await result.blob();
-	  const url = await URL.createObjectURL(blob);
-	  Object.assign(document.createElement('a'), {
-		href: url,
-		download: doc.name + '.conllu',
-	  }).click();
+		let tokenAreaClassName;
+		const isGold = props.quality && props.quality === "gold"
+		if (props.index > 0 && props.probas && props.probas.B > 0.1 && !isGold) {
+			tokenAreaClassName = "token-area token-area-maybe-split"
+		} else {
+			tokenAreaClassName = "token-area"
+		}
+		return (
+			props.index === 0
+				? <span className="" key={props.id} onMouseEnter={enterToken} onMouseLeave={leaveToken}><span className="token">{props.form.value}</span></span>
+				: (
+					<span className={tokenAreaClassName}
+						title="Split sentence"
+						onClick={split}
+						key={props.id}
+						onMouseEnter={enterToken}
+						onMouseLeave={leaveToken}>
+						{(props.index > 0) ? <> <span className="token-button"> <CallSplit fontSize="small" /></span></> : ""}
+						<span className="token">{props.form.value}</span>
+					</span>
+				)
+		)
 	}
 
-	return (
-      <Container maxWidth="md" style={{cursor: busy ? "progress" : "initial"}}>
-        {busy ? <LinearProgress /> : <LinearProgress style={{visibility: "hidden"}} />}
-        <Stack spacing={2} my={5}>
-          {doc === null ? <LoadPlaceholder />
-              : <>
-                {Sentences(doc)}
-                <Button onClick={download} fullWidth>Download</Button>
-              </>}
-        </Stack>
-      </Container>
+  async function download(e) {
+    e.preventDefault();
+    const result = await api.downloadConlluFile(props.id);
+    const blob = await result.blob();
+    const url = await URL.createObjectURL(blob);
+    Object.assign(document.createElement('a'), {
+      href: url,
+      download: doc.name + '.conllu',
+    }).click();
+  }
+
+  return (
+    <Container maxWidth="md" style={{ cursor: busy ? "progress" : "initial" }}>
+      {busy ? <LinearProgress /> : <LinearProgress style={{ visibility: "hidden" }} />}
+      <Stack spacing={2} my={5}>
+        {doc === null ? <LoadPlaceholder />
+          : <>
+            {Sentences(doc)}
+            <Button onClick={download} fullWidth>Download</Button>
+          </>}
+      </Stack>
+    </Container>
   )
 }
 
 function LoadPlaceholder() {
-    return <>
-      <LinearProgress />
-      <Skeleton variant="rectangular" height={24} />
-      <Skeleton variant="rectangular" height={24} />
-      <Skeleton variant="rectangular" height={24} />
-      <Skeleton variant="rectangular" height={24} />
-      <Skeleton variant="rectangular" height={24} />
-    </>;
+  return <>
+    <LinearProgress />
+    <Skeleton variant="rectangular" height={24} />
+    <Skeleton variant="rectangular" height={24} />
+    <Skeleton variant="rectangular" height={24} />
+    <Skeleton variant="rectangular" height={24} />
+    <Skeleton variant="rectangular" height={24} />
+  </>;
 }
